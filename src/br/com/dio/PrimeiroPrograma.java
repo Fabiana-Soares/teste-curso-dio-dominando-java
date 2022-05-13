46c64ddbfa77cfb2fcb612cb20e08cffff6c817c
Fabiana <fsf.fabiana8@gmail.com>
2022-05-13 17:40:19 -0300

    first commit java

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Livro livro = new Livro();
+		
+		System.out.println(livro);
+
+	}
+
+}
+
+class Livro {
+	public String nome;
+}
